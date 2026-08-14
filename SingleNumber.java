b37003c8950bc21194594363badcf8287dd15dfc
smileyharini424-cloud <smileyharini424@gmail.com>
2026-08-13 20:16:30 -0700

    Create SingleNumber.java

---
+++ SingleNumber.java
@@ -0,0 +1,22 @@
+class SingleNumber {
+
+    public static int singleNumber(int[] nums) {
+
+        int result = 0;
+
+        for (int num : nums) {
+            result = result ^ num;
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {4, 1, 2, 1, 2};
+
+        int result = singleNumber(nums);
+
+        System.out.println("Single Number: " + result);
+    }
+}
